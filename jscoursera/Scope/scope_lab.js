d797616c96f607a9408d307285259f91f076bbe5
priopambudi <priopambudi869@gmail.com>
2025-04-05 11:26:44 -0400

    tes

--- jscoursera/Scope/scope_lab.js
+++ jscoursera/Scope/scope_lab.js
@@ -27,6 +27,7 @@ function show() {
 }
 show();
 
+
 console.log(functionVar); // Throws ReferenceError
 console.log(functionLet); // Throws ReferenceError
 console.log(functionConst); // Throws ReferenceError
