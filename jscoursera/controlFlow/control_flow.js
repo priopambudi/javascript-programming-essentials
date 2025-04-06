d270c483b98fad73231c8ffe7e5365f200d3f0a7
priopambudi <priopambudi869@gmail.com>
2025-04-06 05:13:14 -0400

    task practice

--- jscoursera/controlFlow/control_flow.js
+++ jscoursera/controlFlow/control_flow.js
@@ -49,3 +49,40 @@ let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 console.log("Authentication Status:", authenticationStatus);
+
+
+// task
+let usrRole = 'User';
+let isLogin = true;
+let userMsg;
+
+if (isLogin) {
+    if (usrRole === "admin") {
+        userMsg = "Welcome, Admin!";
+    } else {
+        userMsg = "Welcome, User!";
+    }
+} else {
+    userMsg = "Please log in to access the system.";
+}
+
+console.log("User Message:", userMsg);
+
+let usrType = "Employee";
+let userAccess;
+
+switch (usrType) {
+    case "Employee":
+        userAccess = "have access to Dietary Services";
+        break;
+    case "Enrolled Member":
+        userAccess = "have access to Dietary Services and one-on-one interaction with a dietician";
+        break;
+    case "Subscriber":
+        userAccess = "have partial access to facilitate Dietary Services only";
+        break;
+    default:
+        userAccess = "need to enroll or at least subscribe first to avail this facility";
+}
+
+console.log(userAccess)
